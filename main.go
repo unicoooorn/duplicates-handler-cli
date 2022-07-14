d88447fb3fff7e2b285b805c81042a3b5634bbc2
unicoooorn <romanngostilo@gmail.com>
2022-07-14 17:35:27 +0600

    add hashing

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"crypto/md5"
 	"fmt"
+	"hash"
+	"io"
 	"log"
 	"os"
 	"path/filepath"
@@ -25,21 +28,39 @@ func findFilesBySizeExt(rootPath string, ext string) map[int64][]string {
 	return filesOfSize
 }
 
-func printFilesBySize(files map[int64][]string, opt int) {
+func findFilesByHashExt(rootPath string, ext string) map[int64]map[hash.Hash][]string {
+	filesByHash := make(map[int64]map[hash.Hash][]string)
+	if err := filepath.Walk(rootPath, func(path string, info os.FileInfo, err error) error {
+		if len(ext) != 0 && filepath.Ext(path) != ext {
+			return nil
+		}
+		if info.IsDir() {
+			return nil
+		}
+		file, err := os.Open(path)
+		h := md5.New()
+		if _, err := io.Copy(h, file); err != nil {
+			log.Fatal(err)
+		}
+		filesByHash[info.Size()][h] = append(filesByHash[info.Size()][h], path)
+		return nil
+	}); err != nil {
+		log.Fatal(err)
+	}
+	return filesByHash
+}
+
+func printFilesBySize(files map[int64][]string, isAscending bool) {
 	var sizes []int64
 	for size := range files {
 		sizes = append(sizes, size)
 	}
 	sort.Slice(sizes, func(i, j int) bool {
-		switch opt {
-		case 1:
-			return sizes[i] > sizes[j]
-
-		case 2:
+		if isAscending {
 			return sizes[i] < sizes[j]
+		} else {
+			return sizes[i] > sizes[j]
 		}
-		log.Fatal("There is no such option")
-		return true
 	})
 
 	for _, size := range sizes {
@@ -51,32 +72,35 @@ func printFilesBySize(files map[int64][]string, opt int) {
 	}
 }
 
-func main() {
-	if len(os.Args) < 2 {
-		fmt.Println("Directory is not specified")
-		return
+func printFilesByHash(files map[int64]map[hash.Hash][]string, isAscending bool) {
+	var sizes []int64
+	for size := range files {
+		sizes = append(sizes, size)
 	}
-	rootPath := os.Args[1]
+	sort.Slice(sizes, func(i, j int) bool {
+		if isAscending {
+			return sizes[i] < sizes[j]
+		} else {
+			return sizes[i] > sizes[j]
+		}
+	})
 
-	// reading extension to look for (ext is empty = looking for any extension)
-	var ext string
-	fmt.Println("Enter file format:")
-	if _, err := fmt.Scanln(&ext); err != nil {
-		log.Fatal(err)
+	for _, size := range sizes {
+		fmt.Println(size, "bytes")
+		for h := range files[size] {
+			fmt.Println(h)
+			for _, filePath := range files[size][h] {
+				fmt.Println(filePath)
+			}
+		}
+
+		fmt.Println()
 	}
-	fmt.Println()
+}
 
-	// traversing directory
-	filesOfSize := findFilesBySizeExt(rootPath, ext)
-	// sorting menu
-	fmt.Println("Size sorting options:")
-	fmt.Println("1. Descending")
-	fmt.Println("2. Ascending")
-	fmt.Println()
-	// scanning for sorting options
-	isCorrect := false
-	var opt int
-	for !isCorrect {
+func askForSortOpt() bool {
+	opt := 3
+	for opt != 1 && opt != 2 {
 		fmt.Println("Enter a sorting option:")
 		if _, err := fmt.Scan(&opt); err != nil {
 			log.Fatal(err)
@@ -85,14 +109,71 @@ func main() {
 
 		switch opt {
 		case 1:
-			isCorrect = true
-			break
+			return false
 		case 2:
-			isCorrect = true
+			return true
 		default:
 			fmt.Println("Wrong option")
 			fmt.Println()
 		}
 	}
-	printFilesBySize(filesOfSize, opt)
+	return true
+}
+
+func askForHashOpt() bool {
+	opt := ""
+	for opt != "yes" && opt != "no" {
+		fmt.Println("Check for duplicates?")
+		if _, err := fmt.Scan(&opt); err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println()
+
+		switch opt {
+		case "yes":
+			return true
+		case "no":
+			return false
+		default:
+			fmt.Println("Wrong option")
+			fmt.Println()
+		}
+	}
+	return true
+}
+
+func askForExt() string {
+	var ext string
+	fmt.Println("Enter file format:")
+	if _, err := fmt.Scanln(&ext); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println()
+	return ext
+}
+
+func printSortingMenu() {
+	fmt.Println("Size sorting options:")
+	fmt.Println("1. Descending")
+	fmt.Println("2. Ascending")
+	fmt.Println()
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Directory is not specified")
+		return
+	}
+	rootPath := os.Args[1]
+
+	ext := askForExt()
+	printSortingMenu()
+	isAscending := askForSortOpt()
+	filesOfSize := findFilesBySizeExt(rootPath, ext)
+	printFilesBySize(filesOfSize, isAscending)
+	if askForHashOpt() {
+		filesOfHash := findFilesByHashExt(rootPath, ext)
+		printFilesByHash(filesOfHash, isAscending)
+	}
+
 }
